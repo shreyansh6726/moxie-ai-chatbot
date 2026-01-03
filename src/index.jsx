5a2f918519ec279ce6d3a21725ee3d93edcfc966
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-01-03 18:30:24 +0530

    normal commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffectEvent } from 'react';
 import ReactDOM from 'react-dom/client';
 import ChatbotUI from './ChatbotUI';
 import './index.css'; // Global styles and resets
+import { HamIcon, SendHorizonalIcon } from 'lucide-react';
+import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
